58c72b43fb57eb4aa87d2797d6e2e248c449505f
abdullaevbaxrom0000 <abdullaev_baxrom@internet.ru>
2025-03-20 02:29:31 +0500

    разработал кешбек функ28

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['t.me'],
+    },
+  };
+  
+  export default nextConfig;
+  
