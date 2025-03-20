ccdd4508e50cfcfd99c2dd9a799268c4158bf4f3
abdullaevbaxrom0000 <abdullaev_baxrom@internet.ru>
2025-03-20 06:57:48 +0500

    разработал кешбек функ43

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 't.me', // Telegram
-        },
-        {
-          protocol: 'https',
-          hostname: 'lh3.googleusercontent.com', // Google
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 't.me', // Telegram
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Google
+      },
+    ],
+  },
+};
+
+export default nextConfig;
